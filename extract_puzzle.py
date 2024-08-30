da847b867f049958004f01dcfe07081ce8873811
jpfbastos <jpfbastos05@gmail.com>
2024-08-30 23:56:40 +0100

    refactoring

--- extract_puzzle.py
+++ extract_puzzle.py
@@ -12,7 +12,7 @@ def read_puzzle(img, debug=False):
 
     # Blurs the image so noise is diminished
     blurred = cv2.GaussianBlur(img_grey, (9, 9), 0)
-    binary = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 3)
+    binary = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
     # Finds and sorts the contours by area covered
     contours = cv2.findContours(binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
@@ -48,6 +48,7 @@ def read_puzzle(img, debug=False):
         cv2.waitKey(0)
         cv2.imshow("Final binary image", puzzle_binary)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     return puzzle_binary, puzzle_colour
 
@@ -65,26 +66,33 @@ def extract_array(puzzle, debug=False):
             # New image where all pixels with a brightness 200 or above will become white
             # (change second parameter to adjust brightness threshold)
             _, thresh = cv2.threshold(digit, 200, 255, cv2.THRESH_BINARY)
+            if debug:
+                cv2.imshow("Cropped square", digit)
+                cv2.waitKey(0)
             # Everything touching a border which covers 7% (average) of the width or height of the image is cleared
             digit = clear_border(thresh, round((cell_height + cell_width) / 2 * 0.07))
-            if np.average(digit) > 5:  # if the pixel is empty, it will be black (0), so this detects non-black pixels
+            if debug:
+                cv2.imshow("Border cleared", digit)
+                cv2.waitKey(0)
+            if np.average(digit) > 3:  # if the pixel is empty, it will be black (0), so this detects non-black pixels
                 coords = cv2.findNonZero(digit)
                 x, y, w, h = cv2.boundingRect(coords)
                 # Crop the digit from the image
                 cropped_digit = digit[y:y + h, x:x + w]
                 # Resize the digit to make it larger, keeping aspect ratio
-                new_size = (int(40 / h * w), 40)
+                new_size = (int(40 / max(h, w) * min(h, w)), 40)
                 resized_digit = cv2.resize(cropped_digit, new_size, interpolation=cv2.INTER_AREA)
                 digit = np.zeros((50, 50), dtype=np.uint8)
                 # Calculate the position to center the resized digit in the 50x50 image
-                x_offset = (50 - resized_digit.shape[1]) // 2
                 y_offset = (50 - resized_digit.shape[0]) // 2
+                x_offset = (50 - resized_digit.shape[1]) // 2
                 # Place the resized digit into the center of the new 50x50 image
                 digit[y_offset:y_offset + resized_digit.shape[0],
                       x_offset:x_offset + resized_digit.shape[1]] = resized_digit
                 if debug:
-                    cv2.imshow("Digit (press any key to continue)", digit)
+                    cv2.imshow("Final Digit", digit)
                     cv2.waitKey(0)
+                    cv2.destroyAllWindows()
                 digit = cv2.resize(digit, (28, 28))  # For plt.imshow
                 digit = digit.reshape(1, 28, 28, 1)  # For input to model.predict_classes
                 number = extract_digit.extract_number(digit)
--- main.py
+++ main.py
@@ -5,10 +5,76 @@ import extract_puzzle
 import extract_digit
 import solve_puzzle
 
+
+def get_image_from_camera():
+    # Initialize the camera
+    cam = cv2.VideoCapture(0)
+
+    cv2.namedWindow('img')
+
+    while True:
+        # Continuously reads an image from the camera and displays it (basically live video)
+        ret, frame = cam.read()
+        if not ret:
+            print('failed to grab frame')
+            break
+        height, width = frame.shape[0], frame.shape[1]
+        # Makes the frame square (like a sudoku puzzle)
+        start_x, end_x = int(width / 2 - height / 2), int(width / 2 + height / 2)
+        frame = frame[:, start_x:end_x]
+        cv2.imshow('Press Space to take picture, and Esc to quit', frame)
+
+        k = cv2.waitKey(1)
+        if k % 256 == 27:
+            # esc pressed
+            cv2.destroyWindow('Press Space to take picture, and Esc to quit')
+            print('Escape hit, closing...')
+            exit()
+        elif k % 256 == 32:
+            # space pressed
+            cv2.destroyWindow('Press Space to take picture, and Esc to quit')
+            cv2.imshow('Is this a good image? (Press Enter to save, any other key to retake)', frame)
+            k = cv2.waitKey(0)
+            if k % 256 == 13:
+                img = 'sudoku.png'
+                cv2.imwrite(img, frame)
+                print(f'{img} written!')
+                break
+            else:
+                cv2.destroyWindow('Is this a good image? (Press Enter to save, any other key to retake)')
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    return img
+
+
+def solve(img, is_from_camera):
+
+    puzzle, colour = extract_puzzle.read_puzzle(img,
+                                                debug=debug_info == 'read_puzzle' or debug_info == 'all')
+    matrix = extract_puzzle.extract_array(puzzle, debug=debug_info == 'extract_array' or debug_info == 'all')
+    if solve_puzzle.solve_sudoku(matrix):
+        img = solve_puzzle.pretty_print(colour, matrix)
+        if is_from_camera:
+            cv2.imshow('Solved Sudoku (Press Esc to quit, any other key to solve another puzzle)', img)
+        else:
+            #cv2.imshow('Solved Sudoku (Press any key to quit)', img)
+            cv2.imshow('Solved Sudoku (Press Esc to quit, any other key to solve another puzzle)', img)
+        k = cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        if is_from_camera:
+            if k % 256 == 27:  # esc pressed
+                print('Escape hit, closing...')
+                exit()
+    else:
+        print('No solution was found')
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Sudoku Solver from Image")
-    parser.add_argument('--input', '-i', type=str, default=None, help="Path to the input Sudoku image file (leave "
+    parser.add_argument('--source', '-src', type=str, default=None, help="Path to the input Sudoku image file (leave "
                                                                       "blank to use camera)")
     parser.add_argument('--dataset', '-d', type=str, choices=['mnist', 'tmnist'], default='tmnist',
                         help="Choice of dataset for the CNN model ('mnist' or 'tmnist')")
@@ -24,57 +90,9 @@ if __name__ == '__main__':
     debug_info = args.debug
 
     extract_digit.start(dataset_choice)
-
-    while True:
-        # Initialize the camera
-        cam = cv2.VideoCapture(0)
-
-        cv2.namedWindow('img')
-
+    if not input_path:
         while True:
-            # Continuously reads an image from the camera and displays it (basically live video)
-            ret, frame = cam.read()
-            if not ret:
-                print('failed to grab frame')
-                break
-            height, width = frame.shape[0], frame.shape[1]
-            # Makes the frame square (like a sudoku puzzle)
-            start_x, end_x = int(width / 2 - height / 2), int(width / 2 + height / 2)
-            frame = frame[:, start_x:end_x]
-            cv2.imshow('Press Space to take picture, and Esc to quit', frame)
-
-            k = cv2.waitKey(1)
-            if k % 256 == 27:
-                # esc pressed
-                cv2.destroyWindow('Press Space to take picture, and Esc to quit')
-                print('Escape hit, closing...')
-                exit()
-            elif k % 256 == 32:
-                # space pressed
-                cv2.destroyWindow('Press Space to take picture, and Esc to quit')
-                cv2.imshow('Is this a good image? (Press Enter to save, any other key to retake)', frame)
-                k = cv2.waitKey(0)
-                if k % 256 == 13:
-                    img_name = 'sudoku.png'
-                    cv2.imwrite(img_name, frame)
-                    print(f'{img_name} written!')
-                    break
-                else:
-                    cv2.destroyWindow('Is this a good image? (Press Enter to save, any other key to retake)')
-
-        cam.release()
-        cv2.destroyAllWindows()
-
-        puzzle, colour = extract_puzzle.read_puzzle('sudoku.png',
-                                                    debug=debug_info == 'read_puzzle' or debug_info == 'all')
-        matrix = extract_puzzle.extract_array(puzzle, debug=debug_info == 'extract_array' or debug_info == 'all')
-        if solve_puzzle.solve_sudoku(matrix):
-            img = solve_puzzle.pretty_print(colour, matrix)
-            cv2.imshow('Solved Sudoku (Press Esc to quit, any other key to solve another puzzle)', img)
-            k = cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            if k % 256 == 27:  # esc pressed
-                print('Escape hit, closing...')
-                exit()
-        else:
-            print('No solution was found')
+            img_name = get_image_from_camera()
+            solve(img_name, True)
+    else:
+        solve(input_path, False)
